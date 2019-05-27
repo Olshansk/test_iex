89444d3421bbd9111cc523d9ee5b3fb20adb9b02
Daniel Olshansky <olshansky.daniel@gmail.com>
2019-05-26 20:24:20 -0400

    Updated mix.exs

--- mix.exs
+++ mix.exs
@@ -11,17 +11,37 @@ defmodule TestIex.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.18", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    A utility module that helps you iterate faster on unit tests.
+
+    This module lets execute specific tests from within a running iex shell to
+    avoid needing to start and stop the whole application every time.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: [
+        "Daniel Olshansky (olshansky.daniel@gmail.com)"
+      ],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/Olshansk/test_iex"
+      }
     ]
   end
 end
